bd6673205f6e28aa347f9bebfa6032ab79640c20
davronbek2000 <davronbeklatibjonov@gmail.com>
2024-04-11 11:40:02 +0500

    navbar is ready

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -1,1 +1,9 @@
-export const navbar = [];
+import useUniqueId from "../hooks/useId";
+import HomePage from "../pages/Home";
+import PropertiesPage from "../pages/Properties";
+const navbar = [
+  { id: useUniqueId, element: <HomePage />, title: "Home", path: "/home", private: false, hidden: false },
+
+  { id: useUniqueId, element: <PropertiesPage />, title: "Properties", path: "/properties", private: false, hidden: false },
+];
+export default navbar;
