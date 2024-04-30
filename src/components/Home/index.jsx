5aaab6ddc45590763b03b1c9d276dbe643d1b238
davronbek2000 <davronbeklatibjonov@gmail.com>
2024-04-30 18:59:13 +0500

    warning removed

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import {Input, Button} from "../Generic";
 import { Container } from "./style";
 
 const Home = () => {
-  return (
-    <Container>
-    Home
- 
-  </Container>
-  )
+  return <Container>Home</Container>;
 };
 export default Home;
